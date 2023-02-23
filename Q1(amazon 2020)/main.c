8661bdd9aef372596bb0008455bd5293ea05e127
elie-atia <elie-atia@hotmail.fr>
2023-02-23 16:50:30 +0200

    complte q1 of amazon

--- Q1(amazon 2020)/main.c
+++ Q1(amazon 2020)/main.c
@@ -2,187 +2,205 @@
 
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h> 
-#include <time.h> 
+#include <stdlib.h>
+#include <time.h>
 
+#define MEM_LENGTH 10
 
-
-//the arrival packets
-struct packet {
+// the arrival packets
+struct packet
+{
     int priority;
     int arrival_time;
-    struct packet* next;
+    struct packet *next;
 };
 
 int chooseRandoms(int lower, int upper)
 {
     int num = (rand() % (upper - lower + 1)) + lower;
-    //printf("%d ", num); 
     return num;
-
 }
-//funct insert(packet_inter) add the arrival packet_inter to the memory
-int insert(struct packet pck_local,
-    struct packet** heads_arr,
-    struct packet** tails_arr,
-    struct packet* wrt_ptr,
-    struct packet* ptr_to_last_hole,
-    int cnt_avaibalbe_packet,
-    int holes_val,
-    struct packet* memory_packets
-) {
-    int priority_num = pck_local.priority;
-    if (cnt_avaibalbe_packet == 100) {
-        printf("\n          ERROR: PACKET INTERFACE MEMORY IS FULL   \n");
-        return cnt_avaibalbe_packet;
-    }
-    else {
-        cnt_avaibalbe_packet++;
-        if (holes_val != 0) {
-            holes_val--;
-        }
-        struct packet* wrt_ptr_next = wrt_ptr->next;
-        *wrt_ptr = pck_local;
-        printf("\nInserted Packet: time:%d , priority:%d\n", pck_local.arrival_time, pck_local.priority);
-        //update the heads/tails pointers
-        if (heads_arr[priority_num] == NULL) {
-            heads_arr[priority_num] = wrt_ptr;
-            tails_arr[priority_num] = wrt_ptr;
-        }
-        else {
-            tails_arr[priority_num]->next = wrt_ptr;
-            tails_arr[priority_num] = wrt_ptr;
-        }
+// funct insert(packet_inter) add the arrival packet_inter to the memory
+int insert(struct packet pck_to_insert,
+           struct packet **heads_arr,
+           struct packet **tails_arr,
+           struct packet *head_hole,
+           struct packet *tail_hole,
+           int num_of_packet,
+           struct packet *memory_packets)
+{
+    int priority_num = pck_to_insert.priority;
+    struct packet *ptr_to_packet_inserted;
 
-
-        if (holes_val == 0) {
-            wrt_ptr = &memory_packets[cnt_avaibalbe_packet];
-        }
-        else {    //each hole have a pointer to the next hole
-            wrt_ptr = wrt_ptr_next;
-        }
-        return cnt_avaibalbe_packet;
+    // Case: Memory is full => error, exit the function.
+    if (num_of_packet >= MEM_LENGTH)
+    {
+        printf("\n         ERROR: PACKET INTERFACE MEMORY IS FULL   \n");
+        return 0;
     }
+
+    printf("\nInserted Packet: time:%d , priority:%d\n", pck_to_insert.arrival_time, pck_to_insert.priority);
+
+    // Case: No hole in the memrory => insert packet at the first empty place.
+    if (head_hole == NULL)
+    {
+        memory_packets[num_of_packet].arrival_time = pck_to_insert.arrival_time;
+        memory_packets[num_of_packet].priority = pck_to_insert.priority;
+        ptr_to_packet_inserted = &memory_packets[num_of_packet];
+    }
+
+    // Case: There is a single hole in the memory => "fill the hole" and update the hole's ptr.
+    else if (head_hole->next == NULL)
+    {
+        head_hole->arrival_time = pck_to_insert.arrival_time;
+        head_hole->priority = pck_to_insert.priority;
+        ptr_to_packet_inserted = head_hole;
+        head_hole = NULL;
+        tail_hole = NULL;
+    }
+
+    // Case: There is multiple holes in the memory => update the "hole linked list".
+    else
+    {
+        head_hole->arrival_time = pck_to_insert.arrival_time;
+        head_hole->priority = pck_to_insert.priority;
+        ptr_to_packet_inserted = head_hole;
+        head_hole = head_hole->next;
+    }
+
+    // Case: the linked list corresponding to the packet is empty => create it.
+    if (heads_arr[priority_num] == NULL)
+    {
+        heads_arr[priority_num] = ptr_to_packet_inserted;
+        tails_arr[priority_num] = ptr_to_packet_inserted;
+    }
+
+    // Case: the linked list corresponding to the packet exist => update it.
+    else
+    {
+        tails_arr[priority_num]->next = ptr_to_packet_inserted;
+        tails_arr[priority_num] = ptr_to_packet_inserted;
+    }
+
+    return 1;
 }
 
 // funct get_packet() return the packet interface from the memory according to the rules order
-struct packet get_packet(
-    struct packet** heads_arr,
-    struct packet** tails_arr,
-    struct packet* wrt_ptr,
-    struct packet* ptr_to_last_hole,
-    int cnt_avaibalbe_packet,
-    int holes_val,
-    struct packet* memory_packets
-   ) {
-   bool get_succes = 1;
+int get_packet(
+    struct packet **heads_arr,
+    struct packet **tails_arr,
+    struct packet *head_hole,
+    struct packet *tail_hole,
+    struct packet *memory_packets,
+    struct packet *output_packet
+    )
+{
+    int priority_num = -1;
+    output_packet->priority = -1;
+    struct packet *ptr_to_packet_getted;
 
-   struct packet output_packet;
-   int priority_num = -1;
-   
-    if (heads_arr[0] != NULL) {
+
+    // Find the priority num of the "most updated packet".
+    if (heads_arr[0] != NULL)
+    {
         priority_num = 0;
     }
-    else if (heads_arr[1] != NULL){
+    else if (heads_arr[1] != NULL)
+    {
         priority_num = 1;
     }
-    else if (heads_arr[2] != NULL){
+    else if (heads_arr[2] != NULL)
+    {
         priority_num = 2;
     }
-    else if (heads_arr[3] != NULL){
+    else if (heads_arr[3] != NULL)
+    {
         priority_num = 3;
     }
-    else{
-        printf( "\nERROR: YOU TRY TO GET A PACKET, BUT NO PACKET IS AVAIABLE\n");
-        get_succes = 0;
+    else
+    {
+        printf("\nERROR: YOU TRY TO GET A PACKET, BUT NO PACKET IS AVAIABLE\n");
+        return 0;
     }
     
-    if (get_succes == 1) {
-        output_packet.arrival_time = heads_arr[priority_num]->arrival_time;
-        output_packet.priority = heads_arr[priority_num]->priority;
+    output_packet->arrival_time = heads_arr[priority_num]->arrival_time;
+    output_packet->priority = heads_arr[priority_num]->priority;
 
-        if (heads_arr[priority_num] != tails_arr[priority_num]) {
-            heads_arr[priority_num] = heads_arr[priority_num]->next;
-        }
-        else {
-           heads_arr[priority_num] = NULL;
-           tails_arr[priority_num] = NULL;
-        }
-        cnt_avaibalbe_packet--;
-        holes_val++;       
-        if (holes_val > 1) {
-            wrt_ptr->next = heads_arr[priority_num];
-        }
-        wrt_ptr = heads_arr[priority_num];
-        printf("\n   Get   Packet: time:%d , priority:%d\n", output_packet.arrival_time, output_packet.priority);
+    ptr_to_packet_getted = heads_arr[priority_num];
 
+    // Update the linked list of the corresponding to the get packet:
+    if (heads_arr[priority_num] == tails_arr[priority_num])
+    {
+        heads_arr[priority_num] = NULL;
+        tails_arr[priority_num] = NULL;
     }
-    return output_packet;
+    else
+    {
+        heads_arr[priority_num] = heads_arr[priority_num]->next;
+    }
+
+    // Update the hole linked list.
+    if (head_hole == NULL)
+    {
+        head_hole = ptr_to_packet_getted;
+        tail_hole = ptr_to_packet_getted;
+    }
+    else
+    {
+        tail_hole->next = ptr_to_packet_getted;
+        tail_hole = ptr_to_packet_getted;
+    }
+    ptr_to_packet_getted->next = NULL;
+
+    printf("\n   Get   Packet: time:%d , priority:%d\n", output_packet->arrival_time, output_packet->priority);
+
+    return -1;
 }
 
 int main()
 {
-    struct packet memory_packets[100]; //memory array of the packets interface
-//pointers to write/read from the memory
-    struct packet* heads_arr[4], * tails_arr[4], * wrt_ptr = memory_packets, * ptr_to_last_hole = NULL;
-    //the eight-element pointers are considered as follows:
-                                       // [head0,tail0,head1,tail1,...,head3,tail3, wrt_ptr, ptr_to_last_hole]
-    for (int i = 0; i < 4; i++) {
+    struct packet memory_packets[MEM_LENGTH], // Memory array of the packets interface
+                                       // pointers to write/read from the memory.
+        *heads_arr[4], *tails_arr[4], *head_hole = NULL, *tail_hole = NULL,
+                                      pck1, pck2; // packets declarations
+
+    struct packet output_packet[1];
+
+    int num_of_packet = 0;
+
+    for (int i = 0; i < 4; i++)
+    {
         heads_arr[i] = NULL;
         tails_arr[i] = NULL;
     }
-    int holes_val = 0, cnt_avaibalbe_packet = 0;
 
-    /////packets declarations
-    struct packet pck1,pck2;
-
-    //funct insert(packet_inter) add the arrival packet_inter to the memory
-
-    int curr_time = 0, nmbr_or_inserted_packets = 0;
+    // RANDOM SIMULATION:
+    int curr_time = 0;
     srand(time(0));
-    while (nmbr_or_inserted_packets < 320) {
-        printf("%d",cnt_avaibalbe_packet);
-        pck1.priority = chooseRandoms(0, 3);
+    while (curr_time < MEM_LENGTH*300)
+    {
+        pck1.priority = chooseRandoms(0, 1);
         pck1.arrival_time = curr_time;
         curr_time++;
-        printf("try to insert a packet at time : % d with prioriy = % d\n", pck1.arrival_time, pck1.priority);
-        cnt_avaibalbe_packet =  insert(pck1,
+
+        num_of_packet += insert(pck1,
             heads_arr,
             tails_arr,
-            wrt_ptr,
-            ptr_to_last_hole,
-            cnt_avaibalbe_packet,
-            holes_val,
+            head_hole,
+            tail_hole,
+            num_of_packet,
             memory_packets);
-        nmbr_or_inserted_packets++;
 
-        pck2 = get_packet(
-            heads_arr,
-            tails_arr,
-            wrt_ptr,
-            ptr_to_last_hole,
-            cnt_avaibalbe_packet,
-            holes_val,
-            memory_packets);
-            printf("priority: %d, time: %d", pck2.priority, pck2.arrival_time);
-        /*   if (chooseRandoms(0, 1)) {
-               get_packet(
-                  head0,
-                   head1,
-                   head2,
-                 head3,
-                   tail0,
-                   tail1,
-                  tail2,
-                 tail3,
-                   cnt_avaibalbe_packet,
-                  holes_val,
-
-                   wrt_ptr,
-                  ptr_to_last_hole,
-                  memory_packets);
-           }*/
+        if(chooseRandoms(0,1)){
+            num_of_packet += get_packet(
+                heads_arr,
+                tails_arr,
+                head_hole,
+                tail_hole,
+                memory_packets,
+                output_packet);
+        }
     }
-
+    
     return 0;
 }
