68929a0f44e68d5edcae842dd186f7a0b7beccaa
elie-atia <elie-atia@hotmail.fr>
2023-02-28 14:44:30 +0200

    solve Q9

---
+++ Q9 (Mobileye)/main.py
@@ -0,0 +1,19 @@
+def find_all_subsets(l):
+    if(len(l) <= 1 ):
+        return [l]
+
+
+    all_subsets = []
+    for i in range(len(l)):
+        subset_i = [[l[i]]]
+        if(i != len(l)-1):
+            for l2 in  find_all_subsets(l[i+1:]):
+                new_list = l2 + [l[i]] 
+                subset_i.append(new_list)
+
+        all_subsets += subset_i
+
+    return all_subsets
+
+
+print(find_all_subsets([1,2,3,4,5]))
